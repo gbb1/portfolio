6791b686cfb063856e6f75459c2b2dfc44c8d154
Gabe <gbennettbrandt@gmail.com>
2023-06-08 15:33:33 -0500

    Saving

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 import { Tilt } from 'react-tilt';
 import { motion } from 'framer-motion';
@@ -9,6 +9,13 @@ import '../App.css';
 
 
 export default function Project({ prompt, image, tags, flip, title, description, link, bg }) {
+
+  const aRef = useRef(null)
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    if (aRef.current) aRef.current.click();
+  }
 
   return (
     <div className="flex flex-col w-full dark:text-white" >
@@ -41,11 +48,11 @@ export default function Project({ prompt, image, tags, flip, title, description,
           {
             link.length > 0
               ?
-                <button className="z-[10] self-end py-3 px-5 bg-black text-xs md:text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-[#5CDB95] hover:text-black dark:bg-primary-600 dark:hover:bg-primary-700">
-                  <a href={`${link}`} className="w-full h-full">
-                    See details
-                  </a>
-                </button>
+                <a href={`${link}`} ref={aRef} className="z-[10] self-end py-3 px-5 bg-black text-xs md:text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-[#5CDB95] hover:text-black dark:bg-primary-600 dark:hover:bg-primary-700">
+                  <button onClick={handleClick} className="w-full h-full">
+                      See details
+                  </button>
+                </a>
               : <button className="disabled opacity-70 bg-gray-200 self-end py-3 px-5 text-xs md:text-sm font-medium text-center text-black rounded-lg bg-primary-700 sm:w-fit hover:bg-gray-400 hover:text-black dark:bg-primary-600 dark:hover:bg-primary-700">Launching soon!</button>
           }
       </div>
